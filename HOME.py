0619197a81c269f57a5c1959986a380751f30c26
affanm16 <affan24748@gmail.com>
2024-02-09 23:07:34 +0530

    adding new pages

---
+++ HOME.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.sidebar.markdown("HOME")
+st.header(':blue[DETECTION OF ASD USING MACHINE LEARNING]')
+st.subheader('Introduction to Autism Spectrum Disorder (ASD)',divider='blue')
+st.write('Autism Spectrum Disorder (ASD) is a neurodevelopmental condition characterized by challenges with social skills, repetitive behaviors, speech, and nonverbal communication. The spectrum is vast, with each individual having a unique set of strengths and challenges. Understanding the complexities of ASD is crucial for developing effective detection and intervention methods.')
